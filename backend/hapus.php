f1aeea3ff99c107ed2dca30bd3cdf99d74116ac4
Pengguna <tamami.oka@gmail.com>
2022-04-20 13:56:16 +0700

    delete data feature was done

--- backend/hapus.php
+++ backend/hapus.php
@@ -1,6 +1,19 @@
 <?php
+require_once '../connection.php';
+
 $data = $_GET["nim"];
 $obj = new stdClass();
-$obj->status = $data;
+// $obj->status = $data;
+
+$sql = "delete from mahasiswa where nim='" . $data . "'";
+$result = pg_query($sql);
+$row = pg_affected_rows($result);
+
+if($row > 0) {
+    $obj->status = "success";
+} else {
+    $obj->status = "fail";
+}
+
 echo json_encode($obj);
 ?>
